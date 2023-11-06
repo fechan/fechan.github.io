39e685f12e25709a213cdf634a99540b491e8d82
fechan <56131910+fechan@users.noreply.github.com>
2023-11-06 15:11:49 -0800

    enable switching between CV and resume

---
+++ assets/js/resume-switcher.js
@@ -0,0 +1,17 @@
+(function() {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  const cvOnlyElements = document.querySelectorAll(".cv-only");
+  const viewingCVNotice = document.getElementById("viewing-cv-notice");
+  const viewingResumeNotice = document.getElementById("viewing-resume-notice");
+
+  if (urlParams.get("version") === "resume") {
+    cvOnlyElements.forEach(elem => elem.setAttribute("hidden", ""));
+    viewingCVNotice.setAttribute("hidden", "");
+    viewingResumeNotice.removeAttribute("hidden");
+  } else {
+    cvOnlyElements.forEach(elem => elem.removeAttribute("hidden"));
+    viewingCVNotice.removeAttribute("hidden");
+    viewingResumeNotice.setAttribute("hidden", "");
+  }
+})();
